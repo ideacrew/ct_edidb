945d2f6b7c3fa629e686b3c3d7d7f319cc2bb6b8
Trey Evans <tevans@Just-Plain-Awful.local>
2019-05-23 14:55:08 -0400

    Deal with different production access permissions.

--- load_ct_demo_db_prod.sh
+++ load_ct_demo_db_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RAILS_ENV=production bundle exec rake db:drop
+RAILS_ENV=production bundle exec rake db:mongoid:purge
 RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:mongoid:create_indexes
 RAILS_ENV=production bundle exec rake ct_demo:load_data
