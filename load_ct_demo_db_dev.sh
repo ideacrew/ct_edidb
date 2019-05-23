b86bab11b04fa2599332121a7cda0f0b5c83e45c
Zachary Landes <zach.g.landes@gmail.com>
2019-05-23 15:27:24 -0400

    updating to purge

--- load_ct_demo_db_dev.sh
+++ load_ct_demo_db_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bundle exec rake db:drop
+bundle exec rake db:mongoid:purge
 bundle exec rake db:create
 bundle exec rake db:mongoid:create_indexes
 bundle exec rake ct_demo:load_data
